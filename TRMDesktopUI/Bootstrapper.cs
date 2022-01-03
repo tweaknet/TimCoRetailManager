237a2406ed48bf6a0e7c8b4fe533a4cda31f8eca
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-03 20:56:37 +0100

    shell view model

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using TRMDesktopUI.ViewsModels;
+using TRMDesktopUI.ViewModels;
 
 namespace TRMDesktopUI
 {
--- TRMDesktopUI/ViewModels/ShellViewModel.cs
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TRMDesktopUI.ViewsModels
+namespace TRMDesktopUI.ViewModels
 {
     public class ShellViewModel
     {
