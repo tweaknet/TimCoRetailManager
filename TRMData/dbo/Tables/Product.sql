6135b791e450c5626c526d75f7e1bafb2a039afe
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2022-01-05 14:48:20 +0100

    sql tables

---
+++ TRMData/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [ProductName] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [RetailPrice] MONEY NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LasModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
