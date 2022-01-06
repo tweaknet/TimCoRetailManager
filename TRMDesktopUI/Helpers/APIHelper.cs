9b676208cc52a6d76dc4a601ac08eeedf409de6d
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-06 11:37:14 +0100

    apihelp

---
+++ TRMDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.Helpers
+{
+    public class APIHelper
+    {
+        public HttpClient ApiClient { get; set; }
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+        private void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task Authenticate(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type","password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password)
+            });
+            using (HttpResponseMessage response = await ApiClient.PostAsync("", data))
+            {
+
+            }
+        }
+    }
+}
