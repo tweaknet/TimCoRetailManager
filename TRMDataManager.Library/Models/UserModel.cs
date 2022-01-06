2d4c0c0fb528a3e43240bbfa50a9509bc2c0de33
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-06 18:32:31 +0100

    sql ok baza aspnet

--- TRMDataManager.Library/Models/UserModel.cs
+++ TRMDataManager.Library/Models/UserModel.cs
@@ -8,7 +8,7 @@ namespace TRMDataManager.Library.Models
 {
     public class UserModel
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
