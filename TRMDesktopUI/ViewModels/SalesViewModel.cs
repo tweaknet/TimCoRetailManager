df256a5e2dcbc3d90993b5ed3ae0259ff67455e3
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-08 17:39:19 +0100

    18.06

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -49,7 +49,7 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -72,7 +72,12 @@ namespace TRMDesktopUI.ViewModels
         public string SubTotal 
         { get 
             {
-                return "0.00";  
+                decimal subTotal = 0;
+                foreach(var item in _cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C");  
             } }
         public string Tax
         {
@@ -99,13 +104,27 @@ namespace TRMDesktopUI.ViewModels
                 }
                 return output;
             }
-
         }
         public void AddToCart()
         {
-            CartItemModel item = new CartItemModel { Product = SelectedProduct
-            ,QuantityInCart = ItemQuantity};
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if(existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+            }
+            else
+            {
+            CartItemModel item = new CartItemModel 
+            { 
+                Product = SelectedProduct
+                ,QuantityInCart = ItemQuantity
+            };
             Cart.Add(item);
+            }
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+
         }
         public bool CanRemoveFromCart
         {
@@ -119,6 +138,7 @@ namespace TRMDesktopUI.ViewModels
         }
         public void RemoveFromCart()
         {
+            NotifyOfPropertyChange(() => SubTotal);
 
         }
         public bool CanCheckOut
