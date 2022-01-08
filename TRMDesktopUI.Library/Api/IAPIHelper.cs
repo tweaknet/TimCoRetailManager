69f36eec8253d3385762b11d6314857f5d0a88f3
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-08 12:23:09 +0100

    16

--- TRMDesktopUI.Library/Api/IAPIHelper.cs
+++ TRMDesktopUI.Library/Api/IAPIHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 using TRMDesktopUI.Library.Models;
 
 namespace TRMDesktopUI.Library.Api
@@ -7,5 +8,6 @@ namespace TRMDesktopUI.Library.Api
     {
         Task<AuthenticatedUser> Authenticate(string username, string password);
         Task GetLoggedInUserInfo(string token);
+        HttpClient ApiClient { get; }
     }
 }
---
+++ TRMDesktopUI.Library/Api/ProductEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.Library.Api
+{
+    public class ProductEndpoint
+    {
+        public IAPIHelper _apiHelper { get; set; }
+        public ProductEndpoint(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public List<string> GetAll()
+        {
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("/api/Product"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<LoggedInUserModel>();
+                    _loggedInUser.CreatedDate = result.CreatedDate;
+                    _loggedInUser.EmailAddress = result.EmailAddress;
+                    _loggedInUser.FirstName = result.FirstName;
+                    _loggedInUser.LastName = result.LastName;
+                    _loggedInUser.Id = result.Id;
+                    _loggedInUser.Token = token;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
