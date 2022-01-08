699295e265c95afdb14b4b1f6a8fa791817fdcb5
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-08 19:38:41 +0100

    20.03

--- TRMData/dbo/Tables/SaleDetail.sql
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -5,5 +5,7 @@
     [ProductId] INT NOT NULL, 
     [Quantity] INT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NOT NULL, 
-    [Tax] MONEY NOT NULL DEFAULT 0
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSaleTable] FOREIGN KEY (SaleId) REFERENCES Sale(Id), 
+    CONSTRAINT [FK_SaleDetail_ToProductTable] FOREIGN KEY (ProductId) REFERENCES Product(Id)
 )
