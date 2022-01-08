63114b1090dba78344ce38dac1cad218cd4e46e1
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-08 21:06:46 +0100

    20.06 tax calc linq

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -98,13 +98,7 @@ namespace TRMDesktopUI.ViewModels
         {
                 decimal taxAmount = 0;
                 decimal taxRate = _configHelper.GetTaxRate();
-                foreach (var item in Cart)
-                {
-                if (item.Product.IsTaxable)
-                {
-                    taxAmount += (item.Product.RetailPrice * item.QuantityInCart * taxRate / 100); 
-                }
-                }
+            taxAmount = Cart.Where(x => x.Product.IsTaxable).Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate / 100);
                 return taxAmount;
         }
         public string Total
