48891f3c2007c694adde9a3821a0029ced76e4e3
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-09 13:36:51 +0100

    21.06

--- TRMDataManager.Library/DataAccess/SaleData.cs
+++ TRMDataManager.Library/DataAccess/SaleData.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TRMDataManager.Library.Internal.DataAccess;
 using TRMDataManager.Library.Models;
 
 namespace TRMDataManager.Library.DataAccess
 {
     public class SaleData
     {
-        public void SaveSale(SaleModel saleInfo)
+        public void SaveSale(SaleModel saleInfo, string cashierId)
         {
             List<SaleDetailDBModel> details = new List<SaleDetailDBModel>();
             ProductData product = new ProductData();
@@ -38,14 +39,16 @@ namespace TRMDataManager.Library.DataAccess
             SaleDBModel sale = new SaleDBModel
             {
                 SubTotal = details.Sum(x => x.PurchasePrice),
-                Tax = details.Sum(x => x.Tax), 
-
+                Tax = details.Sum(x => x.Tax),
+                CashierId = cashierId
             };
             sale.Total = sale.SubTotal + sale.Tax;
+            // save the sale model
+            SqlDataAccess sql = new SqlDataAccess();
+            sql.SaveData<SaleDBModel>("dbo.spSale_Insert", sale, "EFData");
         }
         //public List<ProductModel> GetProducts()
         //{
-        //    SqlDataAccess sql = new SqlDataAccess();
         //    var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetAll", new { }, "EFData");
         //    return output;
         //}
