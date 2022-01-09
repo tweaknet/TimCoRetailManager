28189365bf13101123e355b7aef1475b068272ba
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-09 14:20:56 +0100

    21.11

--- TRMDataManager.Library/Models/SaleDBModel.cs
+++ TRMDataManager.Library/Models/SaleDBModel.cs
@@ -10,7 +10,7 @@ namespace TRMDataManager.Library.Models
     {
         public int Id { get; set; }
         public string CashierId { get; set; }
-        public DateTime dateTime { get; set; } = DateTime.UtcNow;
+        public DateTime SaleDate { get; set; } = DateTime.UtcNow;
         public decimal SubTotal { get; set; }
         public decimal Tax { get; set; }
         public decimal Total { get; set; }
