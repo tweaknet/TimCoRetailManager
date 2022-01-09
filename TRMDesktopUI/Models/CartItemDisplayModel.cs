776c999eba4b1ffa917efa7ad400a0d9dc4d2a06
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-09 17:03:13 +0100

    22.02

---
+++ TRMDesktopUI/Models/CartItemDisplayModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.Models
+{
+    public class CartItemDisplayModel : INotifyPropertyChanged
+    {
+        public ProductDisplayModel Product { get; set; }
+        private int _quantityInCart;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int QuantityInCart
+        {
+            get { return _quantityInCart; }
+            set 
+            { 
+                _quantityInCart = value;
+                CallPropertyChanged(nameof(QuantityInCart));
+                CallPropertyChanged(nameof(DisplayText));
+            }
+        }
+        private void CallPropertyChanged(string propertyName)
+        {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            throw new NotImplementedException();
+        }
+        public string DisplayText
+        {
+            get
+            {
+                return $"{Product.ProductName}({QuantityInCart})";
+            }
+        }
+    }
+}
---
+++ TRMDesktopUI/Models/ProductDisplayModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.Models
+{
+    public class ProductDisplayModel
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal RetailPrice { get; set; }
+        public int QuantityInStock { get; set; }
+        public bool IsTaxable { get; set; }
+    }
+}
