33aa9faf0d15cd47094781f72c888911e66e9e36
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2022-01-10 14:08:03 +0100

    24.01

--- TRMDesktopUI.Library/Api/IAPIHelper.cs
+++ TRMDesktopUI.Library/Api/IAPIHelper.cs
@@ -9,5 +9,6 @@ namespace TRMDesktopUI.Library.Api
         Task<AuthenticatedUser> Authenticate(string username, string password);
         Task GetLoggedInUserInfo(string token);
         HttpClient ApiClient { get; }
+        void LogOffUser();
     }
 }
--- TRMDesktopUI.Library/Models/LoggedInUserModel.cs
+++ TRMDesktopUI.Library/Models/LoggedInUserModel.cs
@@ -14,7 +14,7 @@ namespace TRMDesktopUI.Library.Models
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public DateTime CreatedDate { get; set; }
-        public void LogOffUser()
+        public void ResetUserModel()
         {
             Token = ""; Id = ""; FirstName = ""; LastName = ""; EmailAddress = ""; CreatedDate = DateTime.MinValue; 
         }
