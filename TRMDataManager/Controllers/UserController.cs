a64b25b854b1a8e938c787ace61fee220d9dc4dc
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2022-01-11 09:50:58 +0100

    29.01

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using TRMDataManager.Library.DataAccess;
 using TRMDataManager.Library.Models;
+using TRMDataManager.Models;
 
 namespace TRMDataManager.Controllers
 {
@@ -17,6 +19,19 @@ namespace TRMDataManager.Controllers
             string userId = RequestContext.Principal.Identity.GetUserId();
             UserData data = new UserData();
             return data.GetUserById(userId).First();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("Admin/GetAllUsers")]
+        public void GetAllUsers()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var users = userManager.Users.ToList();
+                var roles = context.Roles.ToList();
+            }
         }
     }
 }
---
+++ TRMDataManager/Models/ApplicationUserModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TRMDataManager.Models
+{
+    internal class ApplicationUserModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public Dictionary<string, string> Roles { get; set; }
+    }
+}
