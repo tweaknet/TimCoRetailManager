a621d58eb13e0e9662e3cefc4cf3fb570139caea
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2022-01-11 11:31:13 +0100

    29.04

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -44,6 +44,7 @@ namespace TRMDesktopUI
 
             _container.Instance(_container)
                 .PerRequest<IProductEndpoint, ProductEndpoint>()
+                .PerRequest<IUserEndpoint, UserEndpoint>()
                 .PerRequest<ISaleEndpoint, SaleEndpoint>();
 
             _container
---
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.Library.Api;
+using TRMDesktopUI.Library.Models;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class UserDisplayViewModel : Screen
+    {
+        private readonly StatusInfoViewModel _status;
+        private readonly IWindowManager _window;
+        private readonly IUserEndpoint _userEndpoint;
+        BindingList<UserModel> _users;
+        public BindingList<UserModel> Users
+        {
+            get
+            {
+                return _users;
+            }
+            set
+            {
+                _users = value;
+                NotifyOfPropertyChange(() => Users);
+            }
+        }
+
+        public UserDisplayViewModel(StatusInfoViewModel status, IWindowManager window, IUserEndpoint userEndpoint)
+        {
+            _status = status;
+            _window = window;
+            _userEndpoint = userEndpoint;
+        }
+        protected override async void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            try
+            {
+                await LoadUsers();
+            }
+            catch (Exception ex)
+            {
+                dynamic settings = new ExpandoObject();
+                settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                settings.ResizeMode = ResizeMode.NoResize;
+                settings.Title = "System error";
+                if (ex.Message == "Unauthorized")
+                {
+                    _status.UpdateMessage("brak dostępu", "Brak uprawnień sale view model data");
+                    await _window.ShowDialogAsync(_status, null, settings);
+                }
+                else
+                {
+                    _status.UpdateMessage("fatal exception", ex.Message);
+                    await _window.ShowDialogAsync(_status, null, settings);
+                }
+                await TryCloseAsync();
+            }
+        }
+        private async Task LoadUsers()
+        {
+            var userList = await _userEndpoint.GetAll();
+            Users = new BindingList<UserModel>(userList);
+        }
+    }
+}
