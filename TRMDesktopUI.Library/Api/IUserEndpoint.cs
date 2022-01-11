2441d05503d6c1dc932e83661de0825a719b969d
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-11 20:45:02 +0100

    Revert "30.03"
    This reverts commit 43d94157c5da70334a7f4d5af4cf10ea5022fad7.

--- TRMDesktopUI.Library/Api/IUserEndpoint.cs
+++ TRMDesktopUI.Library/Api/IUserEndpoint.cs
@@ -7,8 +7,5 @@ namespace TRMDesktopUI.Library.Api
     public interface IUserEndpoint
     {
         Task<List<UserModel>> GetAll();
-        Task<Dictionary<string, string>> GetAllRoles();
-        Task AddUserToRole(string userId, string roleName);
-        Task RemoveUserFromRole(string userId, string roleName);
     }
 }
