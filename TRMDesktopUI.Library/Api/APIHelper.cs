5daa31e71c6bff189219cf953eb8e577d3cd88f8
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2022-01-13 08:49:25 +0100

    start upgradu

--- TRMDesktopUI.Library/Api/APIHelper.cs
+++ TRMDesktopUI.Library/Api/APIHelper.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using TRMDesktopUI.Library.Models;
 
