37d3738a750441c0f7d16531e70a46d0f50aeab0
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2022-01-14 08:55:37 +0100

    up8

--- TRMApi/Startup.cs
+++ TRMApi/Startup.cs
@@ -31,6 +31,7 @@ namespace TRMApi
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
         }
