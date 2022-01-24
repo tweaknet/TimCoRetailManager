66003020fec554cde049f22d9c6e1eb112ae70b8
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-24 18:55:20 +0100

    57.01

---
+++ Portal/Authentication/JwtParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Portal.Authentication
+{
+    public class JwtParser
+    {
+        public static void ExtractRolesFromJWT(List<Claim> claims, Dictionary<string,object> keyValuePairs)
+        {
+            keyValuePairs.TryGetValue(ClaimTypes.Role, out object role);
+        }
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch(base64.Length % 4)
+            {
+                case 2: 
+                    base64 += "=="; 
+                    break;
+                case 3:
+                    base64 += "=";
+                    break;
+            }
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
