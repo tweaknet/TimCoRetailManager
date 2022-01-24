851d5163f4a6e2ae8285fed32b06a5405423284c
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-24 18:01:34 +0100

    56.02

--- TRMApiv2/Startup.cs
+++ TRMApiv2/Startup.cs
@@ -31,6 +31,12 @@ namespace TRMApiv2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(policy =>
+            {
+                policy.AddPolicy("OpenCorsPolicy", opt => opt.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+            });
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
