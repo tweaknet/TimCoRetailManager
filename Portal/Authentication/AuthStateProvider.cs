b725a6061d7c951504e43a9613b6840be23f6a07
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-24 19:20:23 +0100

    57.06

---
+++ Portal/Authentication/AuthStateProvider.cs
@@ -0,0 +1,24 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Portal.Authentication
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+        private readonly AuthenticationState _anonymous;
+
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsync<string>("authToken");
+        }
+    }
+}
