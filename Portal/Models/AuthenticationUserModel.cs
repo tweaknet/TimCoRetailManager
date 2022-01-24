a5d39654090b7a9d2af97fa5cb073cfff3dc30ea
Marcin Chmielewski <mc@tweaknet.pl>
2022-01-24 19:37:34 +0100

    57.09

---
+++ Portal/Models/AuthenticationUserModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portal.Models
+{
+    public class AuthenticationUserModel
+    {
+        [Required(ErrorMessage = "email is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="passwrod is required.")]
+        public string Password { get; set; }
+    }
+}
