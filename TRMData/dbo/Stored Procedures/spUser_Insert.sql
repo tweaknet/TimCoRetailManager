701abf80ec22b22624000c3f860c9da203b3e730
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2022-01-26 14:16:39 +0100

    61.02

---
+++ TRMData/dbo/Stored Procedures/spUser_Insert.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Insert]
+	@Id nvarchar(128),
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@EmailAddress nvarchar(256)
+AS
+begin
+set nocount on;
+
+
+RETURN 0
