bc073a42769118d7deab9a73cf4271fa940e029f
Marcin Chmielewski <mchmielewski@polcomm.com.pl>
2022-01-27 11:49:47 +0100

    62.02

--- TRMDesktopUI.Library/Models/CreateUserModel.cs
+++ TRMDesktopUI.Library/Models/CreateUserModel.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TRMDesktopUI.Library.Models
 {
-    internal class CreateUserModel
+    public class CreateUserModel
     {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
     }
 }
