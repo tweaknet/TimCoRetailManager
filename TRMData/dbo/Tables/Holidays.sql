69228322e0062d71e499bc356b133592edaff9b6
Marcin Chmielewski <mc@tweaknet.pl>
2022-03-16 14:54:57 +0100

    dodanie tabeli holidays pobieram dane z tabeli pokazuje kto zalogowany email

---
+++ TRMData/dbo/Tables/Holidays.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Holidays]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [IdHolidayType] INT NOT NULL, 
+    [Data] DATETIME2 NOT NULL, 
+    [IdUser] INT NOT NULL, 
+    [HolidayTypeId] INT NOT NULL
+)
